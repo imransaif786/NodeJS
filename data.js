b6679ddd27ab5f85801f6ec937eb9069ef7b209c
Asad Sahi <asadazmat@gmail.com>
2023-02-28 13:46:00 +0000

    refactoring

--- data.js
+++
@@ -1,5 +0,0 @@
-const data = [{name:"imran", age : 40},
-{name:"haseeb", age : 20},
-{name:"ayan", age : 20},
-{name:"abc", age : 20},]
-module.exports = data
--- index.js
+++ index.js
@@ -1,95 +1,135 @@
-const app = require('./app')
-// const fs = require('fs').writeFileSync;
-const http = require('http')
-const color = require('colors')
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/product");
+const Product = require('./product');
+const app = express();
+
+app.use(express.json())
+
+app.get('/list', async (req, resp) => {
+    let data = await Product.find();
+    resp.send(data);
+});
+
+app.post('/create', async (req, resp) => {
+    let data = new Product(req.body)
+    let result = await data.save()
+    console.log(req.body)
+    resp.send('done')
+});
+
+app.put('/update/:_id', async (req, resp) => {
+    let data = await Product.updateOne(
+        req.params,
+        {
+            $set: req.body
+        });
+    resp.send(data);
+});
+
+app.delete('/delete/:_id', async (req, resp) => {
+    let data = await Product.deleteOne(req.params);
+    resp.send("done");
+});
+
+const port = 8888;
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+});
 
 
-// http.createServer((req, resp) => {
-//     resp.writeHead(200, { "Content-Type": "text/plain" });
-//     resp.write("hello im request");
-//     resp.end();
-// }).listen(6500);
-//  console.warn("hello".red);
-// console.log("hello".bgWhite);
 
 
-// Create Simple and first  API Node Js which will get data from data.js file
-// const data = require('./data')
-// const http = require('http')
-// http.createServer((req,resp)=>{
-// resp.writeHead(200,{'content-type' : 'application/json'})
-// resp.write(JSON.stringify(data))
-// resp.end
-// }).listen(6100)
 
-// Crate file and its data from command line  in NODE.JS
-// const fs = require('fs');
-// const input = process.argv;
 
-// // console.log(process.argv);
-// if(input[2] == 'add'){
-//     fs.writeFileSync(input[3],input[4])
-// }else if(input[2] =='remove'){
-//     fs.unlinkSync(input[3])
-// }else{
-//    console.log('invalid input');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+
+// mongoose.set('strictQuery', false);
+
+// const main = async () => {
+//     await mongoose.connect("mongodb://127.0.0.1:27017/e-comm",
+//         {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         }).then(() => {
+//             console.log('connected')
+//         });
+//     const ItemSch = new mongoose.Schema({
+//         name: String
+//     });
+//     const ItemsModel = mongoose.model("Item",ItemSch,"Products1");
+
+//     let data = new ItemsModel({ name: "google" });
+//     let result = await data.save();
+//     console.log(result);
 // }
+// main();
 
+// // const mongoDB = "mongodb://127.0.0.1:27017/e-comm";
 
+// // // Wait for database to connect, logging an error if there is a problem
+// // main().catch(err => console.log(err));
+// // async function main() {
+// //   await mongoose.connect(mongoDB);
+// //   }
+// //   const Schema = mongoose.Schema;
 
-
-// const fs = require('fs')
-// const path = require('path')
-// const dirPath = path.join(__dirname,'files')
-// console.warn(dirPath)
-
-// // for (let i = 0; i < 5; i++) {
-// //     fs.writeFileSync(dirPath+`/apple${i}.txt`,'how are you')
-    
-// // }
-
-// fs.readdir(dirPath,(err,files)=>{
-//     files.forEach((file)=>{
-//         console.log(file);
-//     })
-// })
-
-
-//Creat File with and For CURD operatio
-
-const fs = require('fs');
-const path = require('path');
-const { rejects } = require('assert');
-const dirPath = path.join(__dirname,'curd')
-const filePath = `${dirPath}/apple.txt`
-
-//  fs.writeFileSync(filePath,'this my curd file');
-// this line will read the file content
-// fs.readFile(filePath ,'utf8' ,(err,item)=>{
-//  console.log(item);
-// }) 
-
-
-// fs.appendFile(filePath,' and this part came from appenFile code',(err)=>{
-//     if (!err) console.log('file updated')
-// })
-
-// fs.rename(filePath,`${dirPath}/fruit.txt`,(err)=>{
-//     if(!err) console.log('file name updated');
-// })
-
-// fs.unlinkSync(`${dirPath}/fruit.txt`)
-
-
-// Working with ASYNCHRONOUS Data
-
-// let a = 10;
-
-// let waitingData = new Promise((resolve , reject)=>{
-//     setTimeout(()=>{
-//         resolve(30)
-//     },3000)
-// })
-// waitingData.then((data)=>{
-//     console.log(a+data)
-// })
+// // const SomeModelSchema = new Schema({
+// //   a_string: String,
+// //   a_date: Date,
+// // });
+// // const SomeModel = mongoose.model("SomeModel", SomeModelSchema);
